6cfbe0bdcb9fdddb94e5f787f68535f3900241a0
errornight <night.error.go@gmai;.com>
2023-04-23 22:39:41 +0330

    Modles configured.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from . import models
+from datetime import datetime
+import pytz
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'timesince']
+    search_fields = ['name']
+
+    @staticmethod
+    def timesince(obj):
+        now = datetime.now(pytz.utc)
+        date = obj.date_joined.replace(tzinfo=pytz.utc)
+        elapsed = now - date
+        days = elapsed.days
+        hours, remainder = divmod(elapsed.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        if days > 0:
+            return f"{days} days, {hours} hours ago"
+        elif hours > 0:
+            return f"{hours} hours, {minutes} minutes ago"
+        else:
+            return f"{minutes} minutes ago"
+        
+
+class TasKAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'status']
+    search_fields = ['title', 'description', 'user__username']
+    list_filter = ['status']
+
+admin.site.register(models.User,UserAdmin)
+admin.site.register(models.Task, TasKAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
+import hashlib
 
-# Create your models here.
+def generate_pub_id():
+    return hashlib.sha256(str(instance.id).encode()).hexdigest()
+
+class User(models.Model):
+    # an id for sequrity!
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    # an id for cookies!
+    public_id = models.CharField(max_length=64, unique=True, editable=False)
+    name = models.CharField(max_length=120)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # generate the cookie id
+        if not self.public_id:
+            data = f'{self.id}:{self.name}:{self.date_joined}'
+            self.public_id = hashlib.sha256(data.encode()).hexdigest()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.name.capitalize())
+
+
+class Task(models.Model):
+    STATES = [(True, 'Done'), (False, 'In progress')]
+
+    title = models.CharField(max_length=260)
+    description = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    status = models.BooleanField(choices=STATES, default=False)
+
+    def __str__(self):
+        return str(self.title)
